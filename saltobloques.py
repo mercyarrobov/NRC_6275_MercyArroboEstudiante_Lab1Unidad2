9bcc5767ca78ed5402bfb2e01a9412425b7fe1b9
mercyarrobov <myarrobo@espe.edu.ec>
2022-06-27 23:31:54 -0500

    Final

--- saltobloques.py
+++ saltobloques.py
@@ -1,27 +1,27 @@
 #Importamos las librerias necesarias para poder realizar el aplicativo web
 import os
-import numpy as np
-from flask import Flask, jsonify, make_response, render_template, request
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+from flask import Flask, render_template
 
 #Objeto para inicilizar la aplicacion
 app = Flask(__name__,template_folder='templates/layout')
-#Clave  de la app
-app.secret_key = "123456"
-app.debug = False
+#recuperar nombres de ruta
 app._static_folder = os.path.abspath("templates/static/")
 
 #Controlador de la ruta inicial
 @app.route("/")
+#Función que redirige la página principal
 def index():
+    #restorna la plantilla index.html
     return render_template("index.html")
 
 #Controlador de la ruta inicial
-@app.route("/gamer", methods=["GET"])
+@app.route("/gamer")
+#Función que redirige la segunda página dónde está el juego
 def gamer():
+    #restorna la plantilla saltobloques.html
     return render_template("saltobloques.html")
 
 #Main de la app
 if __name__ == "__main__":
+    #evitar reinicios excesivos del servidor
     app.run(debug=True)
